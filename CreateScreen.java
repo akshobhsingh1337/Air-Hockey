4705ebc74e4fce93a74777c6c9471db715de1afa
Akshobh Singh <a.singh24@lancaster.ac.uk>
2023-05-20 17:46:16 +0100

    Transferred all GameArena objects to CreateScreen Class

--- CreateScreen.java
+++ CreateScreen.java
@@ -1,62 +1,109 @@
 public class CreateScreen {
 
-  private Ball userA;
-  private Ball userB;
-  private Ball blackBall;
-  private GameArena g1;
+  private Ball leftMallet;
+  private Ball rightMallet;
+  private Ball puck;
+  private GameArena gameScreen;
+  private Rectangle whiteScreen;
+  private Line leftGoal;
+  private Line rightGoal;
+  private Rectangle musicBox;
+  private Text topText;
+  private Text leftPlayerScore;
+  private Text rightPlayerScore;
 
   CreateScreen() {
 
-    g1 = new GameArena(1000, 500);
+    gameScreen = new GameArena(1500, 800);
 
-    Rectangle r1 = new Rectangle(0, 0, 1000, 500, "BLUE", 0);
+    Rectangle blackScreen = new Rectangle(0, 0, 1500, 800, "BLACK", 0);
+    Rectangle blueScreen = new Rectangle(250, 150, 1000, 500, "BLUE", 1);
+    whiteScreen = new Rectangle(270, 170, 960, 460, "WHITE", 2);
 
-    Rectangle r2 = new Rectangle(20, 20, 960, 460, "WHITE", 1);
+    gameScreen.addRectangle(blackScreen);
+    gameScreen.addRectangle(blueScreen);
+    gameScreen.addRectangle(whiteScreen);
 
-    g1.addRectangle(r1);
-    g1.addRectangle(r2);
+    leftGoal = new Line(275, 300, 275, 500, 10, "GREY", 3);
+    rightGoal = new Line(1225, 300, 1225, 500, 10, "GREY", 3);
+    Line centerLine = new Line(750, 170, 750, 630, 1, "BLUE", 3);
 
-    Line goal1 = new Line(25, 150, 25, 350, 10, "GREY", 2);
+    gameScreen.addLine(leftGoal);
+    gameScreen.addLine(rightGoal);
+    gameScreen.addLine(centerLine);
 
-    Line goal2 = new Line(975, 150, 975, 350, 10, "GREY", 2);
+    Ball blueCenter = new Ball(750, 400, 100, "BLUE", 4);
+    Ball whiteCenter = new Ball(750, 400, 98, "WHITE", 4);
 
-    Line center = new Line(500, 20, 500, 480, 1, "BLUE", 2);
+    gameScreen.addBall(blueCenter);
+    gameScreen.addBall(whiteCenter);
 
-    g1.addLine(goal1);
-    g1.addLine(goal2);
-    g1.addLine(center);
+    leftMallet = new Ball(400, 400, 50, "BLUE", 5);
+    rightMallet = new Ball(1100, 400, 50, "BLUE", 5);
+    puck = new Ball(750, 400, 25, "BLACK", 5);
 
-    Ball centerA = new Ball(500, 250, 100, "BLUE", 3);
-    Ball centerB = new Ball(500, 250, 98, "WHITE", 3);
+    gameScreen.addBall(leftMallet);
+    gameScreen.addBall(rightMallet);
+    gameScreen.addBall(puck);
 
-    g1.addBall(centerA);
-    g1.addBall(centerB);
+    musicBox = new Rectangle(1300, 600, 172, 30, "GREEN", 1);
+    Text musicText = new Text("Sound Effects", 25, 1303, 622, "BLACK", 3);
 
+    gameScreen.addRectangle(musicBox);
+    gameScreen.addText(musicText);
 
-    userA = new Ball(150, 250, 50, "BLUE", 4);
-    userB = new Ball(850, 250, 50, "BLUE", 4);
-    blackBall = new Ball(500, 250, 25, "BLACK", 4);
+    topText = new Text("Welcome to Air Hockey", 30, 250, 100, "WHITE", 1);
+    leftPlayerScore = new Text(String.valueOf(0), 30, 200, 400, "WHITE", 1);
+    rightPlayerScore = new Text(String.valueOf(0), 30, 1285, 400, "WHITE", 1);
 
-    g1.addBall(userA);
-    g1.addBall(userB);
-    g1.addBall(blackBall);
+    gameScreen.addText(topText);
+    gameScreen.addText(leftPlayerScore);
+    gameScreen.addText(rightPlayerScore);
 
   }
 
-  public Ball getBall1() {
-    return userA;
+  public Ball getLeftMallet() {
+    return leftMallet;
   }
 
-  public Ball getBall2() {
-    return userB;
+  public Ball getRightMallet() {
+    return rightMallet;
   }
 
   public Ball getPuck() {
-    return blackBall;
+    return puck;
   }
 
-  public GameArena getGA() {
-    return g1;
+  public GameArena getGamearena() {
+    return gameScreen;
+  }
+
+  public Rectangle getTable() {
+    return whiteScreen;
+  }
+
+  public Line getLeftGoal() {
+    return leftGoal;
+  }
+
+  public Line getRighGoal() {
+    return rightGoal;
+  }
+
+  public Rectangle getMusicBox() {
+    return musicBox;
+  }
+
+  public Text getTopText() {
+    return topText;
+  }
+
+  public Text getLeftPlayerText() {
+    return leftPlayerScore;
+  }
+
+  public Text getRightPlayerText() {
+    return rightPlayerScore;
   }
 
 }
